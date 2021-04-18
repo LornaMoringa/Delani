d0d56594caedf4ddff1288ce41ae4c5b07fb0369
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-04-18 13:10:08 +0300

    update js

--- js/script.js
+++ js/script.js
@@ -60,3 +60,13 @@ $('document').ready(function () {
     $('#work8').hover(function () {
         $('#giraffe').toggle();
     })
+    $("form#message-form").submit(function () {
+        var name = $("input#input1").val();
+        if ($("input#input1").val() && $("input#input2").val()) {
+            alert(name + ",thank you for reaching out to us. We will get back to you soon.");
+        }
+        else {
+            alert("Please enter your name and email address!");
+        }
+    });
+});
