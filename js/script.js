8edd5479b0e64df50d97478b04b452aeb1761291
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-04-18 12:57:35 +0300

    updated js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ $('document').ready(function () {
         $('#design2').toggle();
         $('#design1').hide();
     })
+    $('#dev1').click(function () {
+        $('#dev2').toggle();
+        $('#dev1').hide();
+    })
+    $('#product1').click(function () {
+        $('#product2').toggle();
+        $('#product1').hide();
+    })
